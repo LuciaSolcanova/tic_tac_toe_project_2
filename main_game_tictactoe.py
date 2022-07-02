ec208204f3c1bc8d89fa1fdd20333b60b7f76431
LuciaSolcanova <lucia.solcanova@post.cz>
2022-07-02 12:54:17 +0200

    Vytvoření úvodního textu

--- main_game_tictactoe.py
+++ main_game_tictactoe.py
@@ -13,3 +13,33 @@ import time
 
 from art_tictactoe import art
 
+
+# ------------- Úvodní text ------------- 
+    # pozdravení uživatele
+    # vypsání pravidel hry
+
+def great():
+    '''Tato funkce pozdraví uživatele a vypíše pravidla hry'''
+
+    print(art)
+
+    separator = '========================================'
+    separator_2 = '--------------------------------------------'
+
+    print('Welcome to Tic Tac Toe')
+    print(separator)
+    print('''GAME RULES:\nEachEach player can place one mark (or stone)
+    per turn on the 3x3 grid. The WINNER is
+    who succeeds in placing three of their
+    marks in a:\n
+    * horizontal,
+    * vertical or
+    * diagonal row''')
+    print(separator)
+    print("Let's start the game")
+    print(separator_2)
+
+# great()
+
+
+
