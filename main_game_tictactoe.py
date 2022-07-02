1b8c0a99105ec1419228f59c622443e5da7d04ff
LuciaSolcanova <lucia.solcanova@post.cz>
2022-07-02 19:50:11 +0200

    Kontrola vítěze

--- main_game_tictactoe.py
+++ main_game_tictactoe.py
@@ -60,14 +60,31 @@ def playing_board(board):
 
 playing_board(board)
 
-# 
+# Výzva hráči, aby umístil kámen na pozici
+
+def player(board):
+    '''Funkce předpokládá, že začíná hráč X'''
+
+    move = 0
+
+    for index in range(3):
+        for i in range(3):
+            if board[index][i] != ' ':
+                move += 1
+
+    if move % 2 == 0:
+        int(input('Player X | Please enter your move number: '))
+    else:
+        int(input('Player O | Please enter your move number:'))
+    
 
 
 
 # Definice funkce pro kontrolu vítěze
 
 def winner(board):
-    
+
+    # vítěz je X
     if (board[0][0] == board[0][1] == board[0][2] == "X" or
         board[1][0] == board[1][1] == board[1][2] == "X" or
         board[2][0] == board[2][1] == board[2][2] == "X" or
@@ -77,8 +94,11 @@ def winner(board):
         board[0][0] == board[1][1] == board[2][2] == "X" or
         board[0][2] == board[1][1] == board[2][0] == "X"):
             
-            return X
-        
+            print(separator)
+            print('Congratulations, the player X WON!')
+            print(separator)
+
+    # vítěz je O
     elif (board[0][0] == board[0][1] == board[0][2] == "O" or
         board[1][0] == board[1][1] == board[1][2] == "O" or
         board[2][0] == board[2][1] == board[2][2] == "O" or
@@ -88,11 +108,14 @@ def winner(board):
         board[0][0] == board[1][1] == board[2][2] == "O" or
         board[0][2] == board[1][1] == board[2][0] == "O"):
             
-            return O
+            print(separator)
+            print('Congratulations, the player O WON!')
+            print(separator)
         
     else:
-        return None
-
+        print(separator)
+        print("It's a draw!")
+        print(separator)
 
 
 
